a17b0d7d409159d73eba83c8a6dd856082f0544d
Alex <alex.svistunov1@gmail.com>
2022-04-03 22:56:20 +0700

    kata 4. task 3 (dry)

--- katas/kata4/kata4/Program.cs
+++ katas/kata4/kata4/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace kata4
 {
@@ -8,7 +9,8 @@ namespace kata4
         public static void Main()
         {
             //weatherTask();
-            soccerTask();
+            //soccerTask();
+            dryTask();
         }
 
         static void weatherTask()
@@ -51,6 +53,70 @@ namespace kata4
             var result = sorted.Where(x=>x.Value == mostMin.Value).ToList();
 
             foreach (var r in sorted)
+            {
+                Console.WriteLine(r.Key + " " + r.Value);
+            }
+        }
+
+        static void dryTask()
+        {
+            string weatherFile = @"C:\Users\user\Downloads\weather.dat";
+            parseTable(
+                weatherFile, 
+                lines => lines.Select(x =>
+                        new KeyValuePair<string, int>(
+                            x[0],
+                            Math.Abs(Convert.ToInt32(x[1].Trim('*')) - Convert.ToInt32(x[2].Trim('*')))
+                        )
+                ),
+                skipFirstRow:true,
+                skipLastRow:true
+            );
+
+            string footballFile = @"C:\Users\user\Downloads\football.dat";
+            parseTable(
+                footballFile,
+                lines => lines.Select(x =>
+                        new KeyValuePair<string, int>(
+                            x[1],
+                            Math.Abs(Convert.ToInt32(x[6]) - Convert.ToInt32(x[8]))
+                        )
+                ),
+                skipFirstRow:true,
+                separators: new[]{'-'}
+            );
+        }
+
+        /*
+            filepath - path to the file
+            action - function to extract needed data
+            separators - array of symbols which used as rows separator
+        */
+        static void parseTable(
+            string filepath,
+            Func<IEnumerable<string[]>, IEnumerable<KeyValuePair<string, int>>> action,
+            bool skipFirstRow = false,
+            bool skipLastRow = false,
+            char[]? separators = null)
+        {
+            var lines = File.ReadAllLines(filepath).AsEnumerable();
+
+            if(skipFirstRow)
+                lines = lines.Skip(1);
+            if(skipLastRow)
+                lines = lines.Take(lines.Count() - 1);
+
+            lines = lines.Where(l => !string.IsNullOrWhiteSpace(l.Trim(separators))); // remove separation rows
+
+
+            var splittedLines = lines.Select(line => Regex.Split(line.Trim(), "\\s+"));
+
+            var filteredData = action(splittedLines).OrderBy(x=>x.Value);
+
+            var mostMin = filteredData.First();
+            var result = filteredData.Where(x => x.Value == mostMin.Value).ToList();
+
+            foreach (var r in result)
             {
                 Console.WriteLine(r.Key + " " + r.Value);
             }
